ecde52b4d03f33aeb4b76d82341957c022422dde
Christopher Ford <caf@slac.stanford.edu>
2016-05-06 13:28:18 -0700

    add LICENSE information, update documentation/README

--- apci1710App/src/apci1710Main.cpp
+++ apci1710App/src/apci1710Main.cpp
@@ -1,5 +1,22 @@
-/* modbusMain.cpp */
-/* Author:  Marty Kraimer Date:    17MAR2000 */
+/**
+ * ----------------------------------------------------------------------------
+ * File       : apci1710Main.cpp
+ * Author     : Marty Kraimer
+ * Created    : 2000-03-17
+ * Last update: 2016-05-06
+ * ----------------------------------------------------------------------------
+ * Description:
+ * Example ioc application.
+ * ----------------------------------------------------------------------------
+ * This file is part of apci1710-asyn. It is subject to 
+ * the license terms in the LICENSE.txt file found in the top-level directory 
+ * of this distribution and at: 
+ *   https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+ * No part of apci1710-asyn, including this file, may be 
+ * copied, modified, propagated, or distributed except according to the terms 
+ * contained in the LICENSE.txt file.
+ * ----------------------------------------------------------------------------
+**/
 
 #include <stddef.h>
 #include <stdlib.h>
--- apci1710App/src/drvApci1710.cpp
+++ apci1710App/src/drvApci1710.cpp
@@ -1,12 +1,23 @@
-/* drvApci1710.cpp
- *
- * Driver for ADDI-DATA APCI1710 incremental counter using asynPortDriver base class
- *
- * This version implements simple counters plus real-time counters
- *
- * Chris Ford
- * March 30, 2016
-*/
+/**
+ * ----------------------------------------------------------------------------
+ * File       : drvApci1710.cpp
+ * Author     : Chris Ford, caf@slac.stanford.edu
+ * Created    : 2016-03-30
+ * Last update: 2016-05-06
+ * ----------------------------------------------------------------------------
+ * Description:
+ * Driver for ADDI-DATA APCI1710 incremental counter using asynPortDriver
+ * base class.  Implements simple (polled) counters plus real-time counters.
+ * ----------------------------------------------------------------------------
+ * This file is part of apci1710-asyn. It is subject to
+ * the license terms in the LICENSE.txt file found in the top-level directory
+ * of this distribution and at:
+ *   https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+ * No part of apci1710-asyn, including this file, may be                       
+ * copied, modified, propagated, or distributed except according to the terms
+ * contained in the LICENSE.txt file.
+ * ----------------------------------------------------------------------------
+**/
 
 #include <stdio.h>
 #include <limits.h>
